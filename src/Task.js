3e7b32ad812f4b59fda5b9562ec0cfaf14e0d9bc
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-08 09:55:17 +0530

    add Todo in Login

--- src/Task.js
+++ src/Task.js
@@ -7,6 +7,14 @@ export default function Task() {
         <div className="row">
           <div className="col-sm-6 mx-auto shadow p-3 rounded">
             <h1 className="text-center">TODO</h1>
+            <div className="row">
+              <div className="col-9">
+                <input type="text" className="form-control"placeholder="Add Your TODO Here" />
+                </div>
+                <div className="col-2">
+                  <button className="ui inverted blue small button"><i class="plus icon"></i> ADD</button>
+                </div>
+            </div>
           </div>
         </div>
       </div>
