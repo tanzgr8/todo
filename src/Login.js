35cb85ea13175ceb44254490514ba4e44541cfde
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-08 09:41:37 +0530

    add major changes

--- src/Login.js
+++ src/Login.js
@@ -8,13 +8,13 @@ export default function Login() {
   const [pass, setPass] = useState("");
   const [emailerr, setemailErr] = useState("Not A Email Valid Format");
   const [passerr, setpassErr] = useState("Too Short Password...Minimum 6 characters");
-  const [todo,setTodo]=useState(false);
-  const Helper=(e)=>{
+  const [todo, setTodo] = useState(false);
+  const Helper = (e) => {
     e.preventDefault();
     notify();
   }
   const notify = () => {
-   
+
     let eiderr = Evalidate();
     let perr = Pvalidate();
     if (eiderr === "false") {
@@ -52,57 +52,66 @@ export default function Login() {
       return "true";
     }
   };
-  function renderbuttons(){
-if(todo){
-  return <Link className="btn btn-success btn-lg" role="button" to="/todo">
-  GO TO TODO APP!
-</Link>; 
-}else{
-  return <button onClick={Helper} className="btn btn-primary btn-lg">
-  Submit
-</button>
-}
+  function renderbuttons() {
+    if (todo) {
+      return (
+        <React.Fragment>
+          <div className="container text-center ct3">
+            <Link className="btn btn-success btn-lg" role="button" to="/todo">
+              GO TO TODO APP!
+            </Link>
+            <ToastContainer />
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      return (<React.Fragment>
+        <div className="container ct1 text-white">
+          <h1>LOGIN</h1>
+          <div className=" ct">
+            <div class="mb-3 row">
+              <label className=" col-sm-2 col-form-label">Email</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  name="email"
+                  className="form-control form-control-sm"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label className=" col-sm-2 col-form-label">Password</label>
+              <div class="col-sm-10">
+                <input
+                  type="password"
+                  name="pass"
+                  className="form-control form-control-sm"
+                  value={pass}
+                  onChange={(e) => {
+                    setPass(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <button onClick={Helper} className="btn btn-primary btn-lg">
+              Submit
+            </button>
+            <ToastContainer />
+          </div>
+        </div>
+      </React.Fragment>
+      );
+    }
   }
 
   return (
-    <React.Fragment>
-      <div className="container ct1 text-white">
-        <h1>LOGIN</h1>
-        <div className=" ct">
-          <div class="mb-3 row">
-            <label className=" col-sm-2 col-form-label">Email</label>
-            <div className="col-sm-10">
-              <input
-                type="text"
-                name="email"
-                className="form-control form-control-sm"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-          </div>
-          <div class="mb-3 row">
-            <label className=" col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-              <input
-                type="password"
-                name="pass"
-                className="form-control form-control-sm"
-                value={pass}
-                onChange={(e) => {
-                  setPass(e.target.value);
-                }}
-              />
-            </div>
-          </div>
 
-         {
-           renderbuttons()
-         }
 
-          <ToastContainer />
-        </div>
-      </div>
-    </React.Fragment>
+
+    renderbuttons()
+
+
   );
 }
